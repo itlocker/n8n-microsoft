333b02e705a95c61d90f19f8105ceb8c0b46ffe7
Matthew Faulkner <matthewfaulkner@Matthews-MacBook-Pro.local>
2024-02-19 15:41:22 +0000

    Comment; testing first resource

--- credentials/MicrosoftOAuth2Api.credentials.ts
+++ credentials/MicrosoftOAuth2Api.credentials.ts
@@ -37,7 +37,7 @@ export class MicrosoftOAuth2Api implements ICredentialType {
 			displayName: 'Authorization URL',
 			name: 'authUrl',
 			type: 'hidden',
-			default: '=https://login.microsoftonline.com/{{"common"}}/oauth2/v2.0/authorize',
+			default: '=https://login.microsoftonline.com/{{$parameter.tenantId}}/oauth2/v2.0/authorize',
 			required: true,
 		},
 		{
